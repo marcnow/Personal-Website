27d2fee20201841cadfe5295c21f08c8747e87a2
Marc Nowakowski <marc.nowakowski@hotmail.com>
2023-01-07 13:50:22 +0100

    fix for landing div

--- src/components/landingpage/LandingDiv.jsx
+++ src/components/landingpage/LandingDiv.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function LandingDiv() {
   return (
-    <div className="pt-10">
+    <div className="pt-20">
       <div className="mb:p-3 lp:p-3">
         <h2>Hey there, my name is Marc</h2>
         <h1>I&apos;m a Software Developer @ adesso SE.</h1>
