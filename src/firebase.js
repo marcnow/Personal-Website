9d1714ba184662c667aae4bae2317e1a8804aba4
Nowakowski, Marc <Marc.Nowakowski@adesso.de>
2023-02-16 19:10:25 +0100

    env

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,6 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(process.env.REACT_APP_API_KEY);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_APP_ID,
